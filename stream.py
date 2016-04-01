fdcd1c3cde1efda392ed3ce11e2589e1e43e4383
Ates Goral <ates.g@myplanet.com>
2016-03-31 20:14:40 -0400

    Blink RGB LED to indicate status

--- stream.py
+++ stream.py
@@ -7,6 +7,8 @@ from subprocess import call
 import yaml
 from TwitterAPI import TwitterAPI, TwitterRequestError, TwitterConnectionError
 
+call([ 'expled', '0000ff' ])
+
 configStream = open(os.path.join(os.path.dirname(__file__), 'config.yml'), 'r')
 config = yaml.load(configStream)
 configStream.close()
@@ -22,8 +24,10 @@ api = TwitterAPI(
 
 while True:
   try:
-    print('requesting');
+    print('Requesting');
     iterator = api.request('user').get_iterator()
+
+    call([ 'expled', '00ff00' ])
 
     for item in iterator:
       if 'text' in item:
@@ -33,27 +37,31 @@ while True:
           if '#ringit' in item['text']:
             out.write("Ringing it!\n")
             call([ 'ubus', 'call', 'gpio', 'set_pin', '{"pin":0,"value":1}' ])
+            call([ 'expled', 'ff00ff' ])
             time.sleep(1)
             call([ 'ubus', 'call', 'gpio', 'set_pin', '{"pin":0,"value":0}' ])
+            call([ 'expled', '00ff00' ])
       elif 'disconnect' in item:
         event = item['disconnect']
 
         if event['code'] in [ 2, 5, 6, 7 ]:
           # something needs to be fixed before re-connecting
+          call([ 'expled', 'ff0000' ])
           raise Exception(event['reason'])
         else:
           # temporary interruption, re-try request
-          print('disconnected, retrying');
+          print('Disconnected, retrying');
           break
   except TwitterRequestError as e:
     if e.status_code < 500:
       # something needs to be fixed before re-connecting
+      call([ 'expled', 'ff0000' ])
       raise
     else:
       # temporary interruption, re-try request
-      print('request error, retrying');
+      print('Request error, retrying');
       pass
   except TwitterConnectionError:
     # temporary interruption, re-try request
-    print('connection error, retrying');
+    print('Connection error, retrying');
     pass
